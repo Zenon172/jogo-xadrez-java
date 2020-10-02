2dfa6a0db4e96e5f40b10c458edc3a16e4f0e347
Lincoln Oliveira <legionario_172@hotmail.com>
2020-10-02 11:50:17 -0300

    Limpar tela e tratamento de exessões

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,9 +1,11 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import xadrez.PartidaXadrez;
 import xadrez.PecaXadrez;
+import xadrez.XadrezException;
 import xadrez.XadrezPosicao;
 
 public class Program {
@@ -14,17 +16,27 @@ public class Program {
 		PartidaXadrez partida = new PartidaXadrez();
 		
 		while (true){
-			UI.printTabuleiro(partida.getPecas());
-			System.out.println();
-			System.out.print("Origem: ");
-			XadrezPosicao origem = UI.LerPosicao(sc);
-			
-			System.out.println();
-			System.out.print("Destino: ");
-			XadrezPosicao destino = UI.LerPosicao(sc);
-			
-			PecaXadrez pecaCapturada = partida.MovimentoXadrez(origem, destino);
-			
+			try{
+				UI.clearScreen();
+				UI.printTabuleiro(partida.getPecas());
+				System.out.println();
+				System.out.print("Origem: ");
+				XadrezPosicao origem = UI.LerPosicao(sc);
+				
+				System.out.println();
+				System.out.print("Destino: ");
+				XadrezPosicao destino = UI.LerPosicao(sc);
+				
+				PecaXadrez pecaCapturada = partida.MovimentoXadrez(origem, destino);
+			}
+			catch (XadrezException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+			catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
 		}
 	}
 
