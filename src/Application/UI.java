57ca3f67aef8a3caa6a63259d0a4f7f29a98d870
Lincoln Oliveira <legionario_172@hotmail.com>
2020-10-05 10:52:12 -0300

    Implementação da classe UI printBackground

--- src/Application/UI.java
+++ src/Application/UI.java
@@ -60,6 +60,18 @@ public class UI {
 		System.out.println("  a b c d e f g h");
 		
 	}
+		public static void printTabuleiro(PecaXadrez[][] pecas, boolean[][] possiveisMovimentos) {
+		
+		for (int i=0; i<pecas.length; i++){
+			System.out.print((8 - i) + " ");
+			for (int j=0; j<pecas.length; j++){
+				printPeca(pecas[i][j], possiveisMovimentos[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.println("  a b c d e f g h");
+		
+	}
 	
 	private static void printPeca(PecaXadrez peca, boolean background) {
 		if (background) {
